9c37bae429428477ca8275ffc45326ffdd7eb289
actatum88 <alexandra.c.tatum@gmail.com>
2023-03-07 12:10:02 -0600

    Adding comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,8 @@
+-- Create a Database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-
+-- Create a User and set password
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
+--Grand privleges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-
+-- Select privleges on database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
