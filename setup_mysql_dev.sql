44ab97b7cc4858f26fecb30d33233fc042e440b4
actatum88 <alexandra.c.tatum@gmail.com>
2023-03-07 13:07:31 -0600

    space

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,7 +7,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Select privleges on database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
---Grant all privleges
+-- Grant all privleges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
