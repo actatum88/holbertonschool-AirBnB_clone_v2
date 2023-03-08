02b8cf20a9e13b09c1d5a9d59310cf06c628c655
Allie Tatum <102764058+actatum88@users.noreply.github.com>
2023-03-08 11:45:46 -0600

    Update setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- Create a User and set password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
---Grant all privleges
+-- Grant all privleges
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- Select privleges on database
