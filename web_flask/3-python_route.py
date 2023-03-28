65ff19796dd20a3fb4323231cab513c524db00d8
actatum88 <alexandra.c.tatum@gmail.com>
2023-03-28 18:37:08 -0500

    Pycodestyle

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,21 +4,26 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello_olleh():
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_confuse(text):
     return "C " + text.replace("_", " ")
 
+
 @app.route('/python/<text>', strict_slashes=False)
 def python_iscool(text):
     return "Python " + text.replace("_", " ")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
