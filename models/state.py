8e086e6bc62c191956465d4346dfd0848fe0e364
actatum88 <alexandra.c.tatum@gmail.com>
2023-03-29 12:49:48 -0500

    update state

--- models/state.py
+++ models/state.py
@@ -18,9 +18,15 @@ class State(BaseModel, Base):
 
     @property
     def cities(self):
-        """return the list of City objects from storage linked to the current State"""
-        city_list = []
-        for city in models.storage.all("City").values():
-            if city.state_id == self.id:
-                city_list.append(city)
-        return city_list
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
