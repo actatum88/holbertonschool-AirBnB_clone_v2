573e1edecbe16d8d03bb9b2ecc9767feb971be75
actatum88 <alexandra.c.tatum@gmail.com>
2023-03-28 19:00:46 -0500

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Start Flask application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_olleh():
+    return 'HBNB'
+
+
+@app.route('/c/')
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_confuse(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def python_iscool(text='is cool'):
+    return "Python " + text.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n) 
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
